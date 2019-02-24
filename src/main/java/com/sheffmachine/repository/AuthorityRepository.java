e97b74983a8b95ed6ebb5ae8acc155c75b48c302
SHEFFmachine <sheff@sheffmachine.com>
2019-02-23 16:24:44 -0800

    Initial application generated by JHipster-5.8.1

---
+++ src/main/java/com/sheffmachine/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.sheffmachine.repository;
+
+import com.sheffmachine.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
